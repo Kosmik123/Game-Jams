6cb6084d7d308a6b3795aadd2ee9460080d00309
Kosmik <malpa.w.klapkach@gmail.com>
2023-09-17 16:50:58 +0200

    Add Camera Fit to project

---
+++ Assets/Scripts/Camera Fit/CameraMainArea.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+public class CameraMainArea : CameraFit
+{
+    [Tooltip("World position of camera view")]
+    [SerializeField]  
+    private Vector2 mainAreaPosition;
+    public Vector2 Position
+    {
+        get => mainAreaPosition;
+        set
+        {
+            mainAreaPosition = value;
+            Refresh();
+        }
+    }
+
+    [Tooltip("Size of camera view (similar to camera orthographic size, but two dimensional)")]
+    [SerializeField]
+    private Vector2 mainAreaSize = new Vector2(4,4);
+    public Vector2 Size
+    {
+        get => mainAreaSize;
+        set
+        {
+            mainAreaSize = value;
+            Refresh();
+        }
+    }
+
+    [Tooltip("Camera view position relative to screen.\n-1 is left. 0 is center. +1 is right.")]
+    [SerializeField] [Range(-1, 1)]
+    private float horizontalShift;
+
+    [Tooltip("Camera view position relative to screen.\n-1 is top. 0 is center. +1 is bottom.")] 
+    [SerializeField] [Range(-1, 1)]
+    private float verticalShift;
+    
+    public Vector2 Shift
+    {
+        get => new Vector2(horizontalShift, verticalShift);
+        set
+        {
+            horizontalShift = Mathf.Clamp(value.x, -1, 1);
+            verticalShift = Mathf.Clamp(value.y, -1, 1);
+            Refresh();
+        }
+    }
+
+    [Tooltip("Zoom of camera view")]
+    [SerializeField]
+    private float zoom = 1;
+    public float Zoom
+    {
+        get => zoom;
+        set
+        {
+            zoom = value;
+            Refresh();
+        }
+    }
+
+    protected override void Resize()
+    {
+        if (camera == null || !camera.orthographic)
+            return;
+
+        float cameraSize = Mathf.Max(mainAreaSize.y, mainAreaSize.x / camera.aspect);
+        cameraSize /= zoom;
+
+        float camWidth = cameraSize * camera.aspect;
+        Vector3 newCamPos = camera.transform.localPosition;
+
+        newCamPos.y = mainAreaPosition.y - (mainAreaSize.y - cameraSize) * verticalShift * 0.5f;
+        newCamPos.x = mainAreaPosition.x + (mainAreaSize.x - camWidth) * horizontalShift * 0.5f;
+
+        ApplyChanges(newCamPos, cameraSize);
+    }
+
+    private void ApplyChanges(Vector3 position, float size)
+    {
+        camera.orthographicSize = 0.5f * size;
+        camera.transform.localPosition = position;
+    }
+
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        if (mainAreaSize.x <= 0)
+            mainAreaSize.x = 0.00001f;
+        if (mainAreaSize.y <= 0)
+            mainAreaSize.y = 0.00001f;
+        if (zoom == 0)
+            zoom = 0.0001f;
+        base.OnValidate();
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(mainAreaPosition, mainAreaSize);
+        base.OnDrawGizmos();
+    }
+#endif
+}
---
+++ Assets/Scripts/Camera Fit/CameraOrthographicSize.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class CameraFit : MonoBehaviour
+{
+    public event Action OnCameraResized;
+
+    [Tooltip("Enable/disable camera changes")]
+    public bool active = true;
+    private float lastAspect;
+    
+    [Tooltip("Camera to resize")]
+    [SerializeField]
+    protected new Camera camera;
+    public Camera Camera
+    {
+        get => camera;
+        set
+        {
+            camera = value;
+            Refresh();
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        Refresh();
+    }
+
+    protected abstract void Resize();
+
+    protected void Refresh()
+    {
+        if (camera == null)
+            return;
+        if (Mathf.Abs(camera.aspect - lastAspect) > 0.01f)
+        {
+            Resize();
+            OnCameraResized?.Invoke();
+        }
+    }
+
+#if UNITY_EDITOR
+    protected virtual void OnDrawGizmos()
+    {
+        if (active)
+            Resize();
+    }
+
+    protected virtual void OnValidate()
+    {
+        if (active)
+            Resize();
+    }
+#endif
+}
+
+public class CameraOrthographicSize : CameraFit
+{
+    [Tooltip("Dimensions which are affected by size changes.\n" +
+        "0 - vertical (same as orthographic size change in Camera)\n" +
+        "1 - horizontal (size affects horizontal dimension of camera orthographic size)\n" +
+        "Anything between (size affect partially both dimensions)")]
+    [Range(0, 1)]
+    public float horizontalFit;
+
+    [Tooltip("Size of orthographic camera (similar to Camera orthographic size")] 
+    public float size;
+
+    protected override void Resize()
+    {
+        if (camera == null)
+            camera = GetComponent<Camera>();
+        if (!camera.orthographic)
+            return;
+
+        camera.orthographicSize = size * ((1 - horizontalFit) + horizontalFit / camera.aspect);
+    }
+}
