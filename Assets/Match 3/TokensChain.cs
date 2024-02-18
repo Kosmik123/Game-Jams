cea167951e3700b8f4606547735f154a3906c528
Michal <malpa.w.klapkach@gmail.com>
2024-02-18 22:39:36 +0100

    Create TriosTokenChain and more abstract TokensChain

--- Assets/Match 3/TokensChain.cs
+++ Assets/Match 3/TokensChain.cs
@@ -6,22 +6,37 @@ namespace Bipolar.Match3
     public class TokensChain
     {
         public TokenType TokenType { get; set; }
-        private readonly HashSet<Vector2Int> tokenCoords = new HashSet<Vector2Int>();
+
+        protected readonly HashSet<Vector2Int> tokenCoords = new HashSet<Vector2Int>();
         public IReadOnlyCollection<Vector2Int> TokenCoords => tokenCoords;
         public bool IsMatchFound { get; set; } = false;
         public bool Contains(Vector2Int tokenCoord) => tokenCoords.Contains(tokenCoord);
         public int Size => tokenCoords.Count;
-
-        private readonly HashSet<Vector2Int> horizontalTrios = new HashSet<Vector2Int>();
-        private readonly HashSet<Vector2Int> verticalTrios = new HashSet<Vector2Int>();
-        public int HorizontalTriosCount => horizontalTrios.Count;
-        public int VerticalTriosCount => verticalTrios.Count;
 
         public void Add(Vector2Int tokenCoord)
         {
             tokenCoords.Add(tokenCoord);
         }
 
+        public virtual void Clear()
+        {
+            TokenType = null;
+            tokenCoords.Clear();
+        }
+
+        public override string ToString()
+        {
+            return $"Tokens Chain ({TokenType.name}): {Size}";
+        }
+    }
+
+    public class TriosTokensChain : TokensChain
+    {
+        private readonly HashSet<Vector2Int> horizontalTrios = new HashSet<Vector2Int>();
+        private readonly HashSet<Vector2Int> verticalTrios = new HashSet<Vector2Int>();
+        public int HorizontalTriosCount => horizontalTrios.Count;
+        public int VerticalTriosCount => verticalTrios.Count;
+        
         public void AddHorizontal(Vector2Int lineCenter)
         {
             horizontalTrios.Add(lineCenter);
@@ -32,17 +47,16 @@ namespace Bipolar.Match3
             verticalTrios.Add(lineCenter);
         }
 
-        public void Clear()
+        public override void Clear()
         {
-            TokenType = null;
-            tokenCoords.Clear();
+            base.Clear();
             verticalTrios.Clear();
             horizontalTrios.Clear();
         }
 
         public override string ToString()
         {
-            return $"Tokens Chain ({TokenType.name}): {Size}, H: {HorizontalTriosCount}, V: {VerticalTriosCount}";
+            return base.ToString() + $", H: {HorizontalTriosCount}, V: {VerticalTriosCount}";
         }
     }
 }
